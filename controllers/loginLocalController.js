e7f43da63dca086a3238cc9cf780248413eace39
huan206 <huan.nguyen2261@gmail.com>
2022-01-17 12:54:56 +0700

    forgot password function

--- controllers/loginLocalController.js
+++ controllers/loginLocalController.js
@@ -2,6 +2,8 @@ var User = require('../models/user');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var bcrypt = require('bcrypt');
+var mailer = require('../config/mailer');
+const { body, validationResult } = require('express-validator');
 
 passport.use(new LocalStrategy(
     function (username, password, done) {
@@ -26,4 +28,52 @@ passport.use(new LocalStrategy(
 
 exports.login = function (req, res) {
     res.render('login_views/login.ejs', { title: 'Login to Account' });
+};
+
+exports.sendmailFogot = [
+    body('email').trim().isLength({ min: 1 }).escape().withMessage('Family name must be specified.')
+        .withMessage('Email has non-alphanumeric characters.'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.render('register.ejs', { title: 'Create Account', account: req.body, errors: errors.array() });
+            return;
+        }
+        else {
+            
+            User.findOne({
+                email: req.body.email
+            })
+                .then(result => {
+                    if(result){
+                        code = Math.floor(Math.random() * (999999 - 100000)) + 100000;
+                        console.log(code);
+                        // mailer.sendMail(req.body.email, code);
+                        res.render('login_views/verifyforgot.ejs', { title: 'Verify Account', account: result })
+                    }
+                    else{
+                        res.render('login_views/forgotpassword.ejs',{title: 'Forgot Password', err:'The email does not exist'})
+                    }
+                }
+                )
+
+        }
+    }
+];
+
+exports.updatePassword = (req, res, next) => {
+    console.log(req.body);
+    if (req.body.code == code && req.body.confirm == req.body.password) {
+        bcrypt.hash(req.body.password, 10, function (err, hash) {
+            User.findByIdAndUpdate(req.body.id, { $set: { password: hash } }, {}, function (err) {
+                if (err) { return next(err); }
+                res.redirect("/login");
+            });
+
+        })
+    }
+    else {
+        res.render('login_views/verifyforgot.ejs', { title: 'Verify Account', account: req.body })
+    }
 };
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var passport = require('passport');
 var local_controller = require('../controllers/loginLocalController');
 var user_controller = require('../controllers/userController');
 
+// Login
 router.get('/', user_controller.checkAuthenticated, user_controller.homepage);
 
 router.get('/login', user_controller.checkNotAuthenticated, local_controller.login);
@@ -14,6 +15,16 @@ router.post('/login',
     res.redirect('/');
   });
 
-router.get('/logout', user_controller.logout );
+// Forgot Password
+router.get('/sendmail_forgot', function (req, res, next) {
+  res.render('login_views/forgotpassword.ejs', { title: "Forgot Password", err: undefined });
+})
+
+router.post('/sendmail_forgot', local_controller.sendmailFogot);
+
+router.post('/new_password', local_controller.updatePassword);
+
+// Logout
+router.get('/logout', user_controller.logout);
 
 module.exports = router;

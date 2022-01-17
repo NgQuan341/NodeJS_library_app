55da34313840a5bcf9059b5f00e30d4e836752be
NgQuan341 <nghongquan34@gmail.com>
2022-01-17 11:27:06 +0700

    login function

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var local_controller = require('../controllers/loginLocalController');
+var user_controller = require('../controllers/userController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', user_controller.checkAuthenticated, user_controller.homepage);
+
+router.get('/login', user_controller.checkNotAuthenticated, local_controller.login);
+
+router.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
+  function (req, res) {
+    res.redirect('/');
+  });
+
+router.get('/logout', user_controller.logout );
 
 module.exports = router;
